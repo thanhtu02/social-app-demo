ca0a07d0a08270e0c886381bf49a56b24165ea37
thanhtu02 <hanhee1406@gmail.com>
2023-11-15 12:23:09 +0700

    ok

--- pages/api/flutter/like/index.js
+++ pages/api/flutter/like/index.js
@@ -1,31 +1,30 @@
 import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async function handler(req, res) {
-  const { accessToken } = await getAccessToken(req, res);
-  const fetchOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Request-Headers": "*",
-      jwtTokenString: accessToken,
-    },
-  };
-  const fetchBody = {
-    dataSource: process.env.MONGODB_DATA_SOURCE,
-    database: "social_butterfly",
-    collection: "flutters",
-  };
-  const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
+    const { accessToken } = await getAccessToken(req, res);
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Request-Headers": "*",
+            jwtTokenString: accessToken,
+        },
+    };
+    const fetchBody = {
+        dataSource: process.env.MONGODB_DATA_SOURCE,
+        database: "social_butterfly",
+        collection: "flutters",
+    };
+    const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
 
-  try {
-    switch (req.method) {
-      // Add Update Functionality Here
-      default:
-        res.status(405).end();
-        break;
+    try {
+        switch (req.method) {
+            // Add Update Functionality Here
+            default: res.status(405).end();
+            break;
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error });
-  }
 });
